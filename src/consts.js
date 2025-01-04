372e9ca2b3c579f450a400592cd3bb3685056e49
shilianmalaxiangguo <695614960@qq.com>
2025-01-04 16:17:44 +0800

    feat: init css

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const SITE_TITLE = `保安's博客`;
 export const SITE_DESCRIPTION = '成都富婆加我';
-export const SITE_EMAIL = 'shilianmlxg@foxmail.com'
+export const SITE_EMAIL = 'guangchaozeng@foxmail.com'
 export const SITE_NAME = '保安';
-export const SITE_URL = "https://ovorange.vercel.app";
+export const SITE_URL = "https://ovorange.pages.dev";
